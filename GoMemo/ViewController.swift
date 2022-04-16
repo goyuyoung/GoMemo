47e8e30f41adbe7d604a2d822cbcbe256adb290a
goyuyoung <yooou5369@naver.com>
2022-04-16 16:13:56 +0900

    Initial Commit

---
+++ GoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoMemo
+//
+//  Created by 고유영 on 2022/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
