dd3772dada59f08c17d864e763ede762a90065b3
goyuyoung <yooou5369@naver.com>
2022-04-25 23:26:01 +0900

    새 메모 작성 화면 추가

---
+++ GoMemo/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  GoMemo
+//
+//  Created by 고유영 on 2022/04/25.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
